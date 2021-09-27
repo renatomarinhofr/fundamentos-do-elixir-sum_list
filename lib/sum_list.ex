c044c22433c246e4545f2cb797a118e6132ed1c5
Renato Marinho <renato.marinhofr@gmail.com>
2021-09-27 01:02:45 -0300

    testando a aplicação

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -9,7 +9,7 @@ defmodule SumList do
   end
 end
 
-# [1,2,3]
+# [1,2,3], 0
 
 # 1 ex: [1,2,3] hd: 1, tail: [2,3], 0 ->  acc = 0 + 1, sum([2,3], 1)
 # 2 ex: [2,3] hd: 2, tail: [3], 1 -> acc = 1 + 2, sum([3], 3)

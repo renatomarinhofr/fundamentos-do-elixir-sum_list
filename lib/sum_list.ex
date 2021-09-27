4c5067ec289f9df5032371651a42d636e78b2b89
Renato Marinho <renato.marinhofr@gmail.com>
2021-09-27 00:51:59 -0300

    Somando todos elementos de uma lista recursivamen

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,9 +1,17 @@
 defmodule SumList do
-  def sum([], acc) do
-    0
-  end
+  def call(list), do: sum(list, 0)
 
-  def sum([head | tail] = list, acc) do
-    list
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
   end
 end
+
+# [1,2,3]
+
+# 1 ex: [1,2,3] hd: 1, tail: [2,3], 0 ->  acc = 0 + 1, sum([2,3], 1)
+# 2 ex: [2,3] hd: 2, tail: [3], 1 -> acc = 1 + 2, sum([3], 3)
+# 3 ex: [3] hd: 3, tail: [], 3 -> acc = 3 + 3, sum([], 6)
+# 4 ex: [] acc = 6
